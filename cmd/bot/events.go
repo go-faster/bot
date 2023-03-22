b4eafe3819463ddd31d7ede1b919a82ad838cf3a
Aleksandr Razumov <ernado@ya.ru>
2023-03-22 23:38:26 +0300

    feat: ev

--- cmd/bot/events.go
+++ cmd/bot/events.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"time"
 
 	"github.com/ClickHouse/ch-go"
 	"github.com/ClickHouse/ch-go/proto"
@@ -12,19 +13,31 @@ import (
 
 func (a *App) HandleEvents(ctx context.Context, e dispatch.MessageEvent) error {
 	var count proto.ColUInt64
-	q := ch.Query{
+	if err := a.ch.Do(ctx, ch.Query{
 		Body: "SELECT COUNT() FROM faster.github_events_raw WHERE ts > now() - toIntervalMinute(30)",
 		Result: proto.Results{
 			{Name: "count()", Data: &count},
 		},
-	}
-	if err := a.ch.Do(ctx, q); err != nil {
+	}); err != nil {
 		return errors.Wrap(err, "do")
 	}
 	if len(count) == 0 {
 		return errors.New("no data")
 	}
-	if _, err := e.Reply().Textf(ctx, "Events in last 30 minutes: %d", count[0]); err != nil {
+	var max, min proto.ColDateTime
+	if err := a.ch.Do(ctx, ch.Query{
+		Body: "SELECT max(ts), min(ts) FROM faster.github_events_raw;",
+		Result: proto.Results{
+			{Name: "max(ts)", Data: &max},
+			{Name: "min(ts)", Data: &min},
+		},
+	}); err != nil {
+		return errors.Wrap(err, "do")
+	}
+	duration := max.Row(0).Sub(min.Row(0))
+	if _, err := e.Reply().Textf(ctx, "Events in last 30 minutes: %d\nSize: %s",
+		count[0], duration.Truncate(1*time.Second),
+	); err != nil {
 		return errors.Wrap(err, "reply")
 	}
 
