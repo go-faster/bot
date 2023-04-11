e012dd4649ba8842f80986059e85f2a07547e626
Aleksandr Razumov <ernado@ya.ru>
2023-04-11 13:55:18 +0300

    fix: otel

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,18 +3,12 @@ package main
 import (
 	"context"
 
-	"go.opentelemetry.io/otel/semconv/v1.18.0"
 	"go.uber.org/zap"
 
 	"github.com/go-faster/bot/internal/app"
-	"github.com/go-faster/bot/internal/otelenv"
 )
 
 func main() {
-	otelenv.Set(
-		semconv.ServiceNameKey.String("bot"),
-		semconv.ServiceNamespaceKey.String("faster"),
-	)
 	app.Run(func(ctx context.Context, lg *zap.Logger, m *app.Metrics) error {
 		return runBot(ctx, m, lg.Named("bot"))
 	})
