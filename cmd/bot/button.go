2c58f8104f2406ea35caece798b96b88530ceaaa
Aleksandr Razumov <ernado@ya.ru>
2023-04-11 15:14:41 +0300

    feat: action

--- cmd/bot/button.go
+++ cmd/bot/button.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/gotd/td/tg"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.uber.org/zap"
 
+	"github.com/go-faster/bot/internal/action"
 	"github.com/go-faster/bot/internal/dispatch"
 )
 
@@ -22,6 +25,27 @@ func (a *App) OnButton(ctx context.Context, e dispatch.Button) error {
 	a.lg.Info("OnButton",
 		zap.String("user", e.Input.String()),
 	)
+
+	var act action.Action
+	if err := json.Unmarshal(e.Data, &act); err != nil {
+		answer := &tg.MessagesSetBotCallbackAnswerRequest{
+			QueryID: e.QueryID,
+			Message: fmt.Sprintf("Error: %s", err),
+			Alert:   true,
+		}
+		if _, err := e.RPC().MessagesSetBotCallbackAnswer(ctx, answer); err != nil {
+			return err
+		}
+		span.SetStatus(codes.Error, err.Error())
+		return nil
+	}
+
+	span.SetAttributes(
+		attribute.Int("action.id", act.ID),
+		attribute.String("action.entity", act.Entity),
+		attribute.String("action.action", act.Action),
+	)
+
 	answer := &tg.MessagesSetBotCallbackAnswerRequest{
 		QueryID:   e.QueryID,
 		Message:   fmt.Sprintf("Hello, %s!", e.User.FirstName),
@@ -30,5 +54,6 @@ func (a *App) OnButton(ctx context.Context, e dispatch.Button) error {
 	if _, err := e.RPC().MessagesSetBotCallbackAnswer(ctx, answer); err != nil {
 		return err
 	}
+
 	return nil
 }
