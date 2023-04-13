f218607f8d25569b0818d7c6e5960874fdc7f2a4
Aleksandr Razumov <ernado@ya.ru>
2023-04-13 18:01:00 +0300

    chore: increase featchevents ticker time even more

--- cmd/bot/app.go
+++ cmd/bot/app.go
@@ -225,7 +225,7 @@ func (a *App) Run(ctx context.Context) error {
 		})
 	}
 	g.Go(func() error {
-		ticker := time.NewTicker(time.Second * 15)
+		ticker := time.NewTicker(time.Minute * 2)
 		defer ticker.Stop()
 		for {
 			select {
