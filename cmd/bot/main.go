eae374c37147728f83c3f8fe43dcac449668e768
Aleksandr Razumov <ernado@ya.ru>
2023-04-16 04:58:39 +0300

    chore: try more logs

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -9,6 +12,13 @@ import (
 )
 
 func main() {
+	println("start")
+	defer func() {
+		if re := recover(); re != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "panic: %v", re)
+		}
+		time.Sleep(time.Second * 3)
+	}()
 	app.Run(func(ctx context.Context, lg *zap.Logger, m *app.Metrics) error {
 		return runBot(ctx, m, lg.Named("bot"))
 	})
