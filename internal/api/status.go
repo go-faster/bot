94b994a038db1c71bf396f43fb00a0e4791a03d5
Aleksandr Razumov <ernado@go-faster.org>
2023-06-04 10:12:34 +0300

    feat(api.status): only count last week

--- internal/api/status.go
+++ internal/api/status.go
@@ -2,14 +2,20 @@ package api
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-faster/errors"
 
+	"github.com/go-faster/bot/internal/ent/gitcommit"
 	"github.com/go-faster/bot/internal/oas"
 )
 
 func (s *Server) Status(ctx context.Context) (*oas.Status, error) {
-	totalCommits, err := s.db.GitCommit.Query().Count(ctx)
+	// Last week.
+	until := time.Now().AddDate(0, 0, -7)
+	totalCommits, err := s.db.GitCommit.Query().
+		Where(gitcommit.DateGTE(until)).
+		Count(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "count commits")
 	}
